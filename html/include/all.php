c71222ce1533ab794242a4342775408867486e64
Lukasdotcom <72501726+Lukasdotcom@users.noreply.github.com>
2021-05-12 15:56:43 -0400

    Clarity has wrong code

--- html/include/all.php
+++ html/include/all.php
@@ -166,7 +166,7 @@ echo '<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-ic
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "6ngv933hwr");
+    })(window, document, "clarity", "script", "6nguibuvtp");
 </script>';
 // Removes all expired cookies from the database
 $Time = mkTime();
