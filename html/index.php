055021a26db8f2f194ad7765d5cfde3c1e201ec1
Lukasdotcom <72501726+Lukasdotcom@users.noreply.github.com>
2021-05-12 09:31:41 -0400

    Removes unnecessary "

--- html/index.php
+++ html/index.php
@@ -16,7 +16,7 @@
     include 'include/menu.php';
     ?>
     <div class='main'>
-        <h1>Welcome to the Schaefer Family website!</h1>"
+        <h1>Welcome to the Schaefer Family website!</h1>
         <a href="/floppy.php">
             <img src="/favicon/android-chrome-512x512.png">
         </a>
