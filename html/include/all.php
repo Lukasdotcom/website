af1d48616582f6d9740bbb096927a8eed9b70cf5
Lukasdotcom <72501726+Lukasdotcom@users.noreply.github.com>
2021-05-11 21:16:47 -0400

    Added clarity from microsoft

--- html/include/all.php
+++ html/include/all.php
@@ -160,7 +160,14 @@ echo '<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-ic
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
     <link rel="manifest" href="/favicon/site.webmanifest">
-    <link rel="stylesheet" type="text/css" href="/css/website.css" />';
+    <link rel="stylesheet" type="text/css" href="/css/website.css" />
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "6ngv933hwr");
+</script>';
 // Removes all expired cookies from the database
 $Time = mkTime();
 dbRemove("cookies", "expire", $Time, 1);
