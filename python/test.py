eb91d90f3e475a4a6e9b645f19a8795d4b229271
lukasdotcom <lukas.aurelius@outlook.com>
2021-06-09 11:13:47 -0400

    deleted accidently created file

--- python/test.py
+++
@@ -1,7 +0,0 @@
-import json, os
-def readFile(location):
-    with open(location) as f:
-        return json.load(f)
-
-
-print(readFile(__file__[:__file__.rindex("/")+1]+"config.json")["websiteRoot"]+"/")
