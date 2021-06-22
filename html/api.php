5ce22797d3c760f0f8edca43b977ed0e987b1760
Lukasdotcom <lukas.aurelius@outlook.com>
2021-06-22 10:31:28 -0400

    Makes the api more clear to the user what it does

--- html/api.php
+++ html/api.php
@@ -7,20 +7,34 @@ if($_GET["internet"] == "data") {
     $data = dbRequest2("SELECT * FROM internet ORDER BY id Asc");
     echo json_encode($data);
 } elseif ($PRIVILEGE["internet"] and $_GET["internet"] === "button"){
-    echo "Unsupported";
-} elseif ($PRIVILEGE["internet"] and $_GET["internet"] === "edit") {
-    $id = intval($_GET["id"]);
-    $startHour = intval($_GET["startHour"]);
-    $startMinute = intval($_GET["startMinute"]);
-    $endHour = intval($_GET["endHour"]);
-    $endMinute = intval($_GET["endMinute"]);
-    $expire = intval($_GET["expire"]);
-    if (dbRequest2("SELECT * FROM internet WHERE id='$id'")) {
-        dbEdit("internet", [["hour", $startHour], ["minute", $startMinute], ["hour2", $endHour], ["minute2", $endMinute], ["expire", $expire]], ["id", $id], 0);
+    echo "Unsupported. This message will eventually be removed";
+    http_response_code(410);
+} elseif ($_GET["internet"] === "edit") {
+    if ($PRIVILEGE["internet"]) {
+        $id = intval($_GET["id"]);
+        $startHour = intval($_GET["startHour"]);
+        $startMinute = intval($_GET["startMinute"]);
+        $endHour = intval($_GET["endHour"]);
+        $endMinute = intval($_GET["endMinute"]);
+        $expire = intval($_GET["expire"]);
+        if (dbRequest2("SELECT * FROM internet WHERE id='$id'")) {
+            dbEdit("internet", [["hour", $startHour], ["minute", $startMinute], ["hour2", $endHour], ["minute2", $endMinute], ["expire", $expire]], ["id", $id], 0);
+        } else {
+            dbAdd([$startHour, $startMinute, $endHour, $endMinute, $expire, $id], "internet");
+        }
     } else {
-        dbAdd([$startHour, $startMinute, $endHour, $endMinute, $expire, $id], "internet");
+        http_response_code(401);
+        var_dump("Forbidden");
     }
-} elseif ($PRIVILEGE["internet"] and $_GET["internet"] === "delete") {
-    $id = intval($_GET["id"]);
-    dbCommand("DELETE FROM internet WHERE id='$id'");
+} elseif ($_GET["internet"] === "delete") {
+    if ($PRIVILEGE["internet"]) {
+        $id = intval($_GET["id"]);
+        dbCommand("DELETE FROM internet WHERE id='$id'");
+    } else {
+        http_response_code(401);
+        var_dump("Forbidden");
+    }
+} else {
+    http_response_code(400);
+    var_dump("Invalid command");
 }
