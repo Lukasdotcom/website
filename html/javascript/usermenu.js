05919dee69380636f22ad3bd536e394658326816
Lukasdotcom <lukas.aurelius@outlook.com>
2021-07-13 19:51:03 -0400

    start of changes

---
+++ html/javascript/usermenu.js
@@ -0,0 +1,6 @@
+function editUserStart(username) { // startup function to make the page look correct
+    document.getElementById("user").value = username;
+}
+function userInfo(username) {
+    document.getElementById
+}
