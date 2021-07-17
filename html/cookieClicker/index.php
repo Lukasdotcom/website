879954ace8edb282ad02155167d38278726cdf9b
Lukasdotcom <lukas.aurelius@outlook.com>
2021-07-17 08:55:22 -0400

    Updates descriptions and adds a picture

--- html/cookieClicker/index.php
+++ html/cookieClicker/index.php
@@ -7,7 +7,7 @@
         Schaefer Family
     </title>
     <?php
-    $DESCRIPTION = "Cookie clicker mod located here";
+    $DESCRIPTION = "Cookie clicker addon located here";
     require_once '../include/all.php';
     ?>
 </head>
@@ -17,12 +17,17 @@
     require_once '../include/menu.php';
     echo "<div class='main'>";
     ?>
-    <p>javascript:(function() {
-    var script = document.createElement('script');
-    script.src = "http://localhost/cookieClicker/index.js";
-    document.head.appendChild(script);
-    }());</p>
-    </div>
+    <h1>Welcome to the main page for the cookie clicker addon</h1>
+    <p>This add on allows you to have a multiplayer cookie clicker expirience. To use it you just have to create a bookmark that has the text in the box below. Then go to the cookie clicker website and click on it.</p>
+    <span style="border-width:3px;border-style:solid;padding:5px;border-color:green;">javascript:var script = document.createElement('script');
+    script.src = "<?php
+    // Will echo out the hostname
+    echo $_SERVER["REQUEST_SCHEME"] . '://' . $_SERVER["HTTP_HOST"];?>
+    /cookieClicker/index.js";
+    document.head.appendChild(script);</span>
+    <p>After that you just have to pick a room if you want to use a public room the closest to that is just not entering anything for the room</p>
+    <img src='/img/cookieClicker.png' alt='Picture of the addon' width="100%">
+</div>
 </body>
 
 </html>
