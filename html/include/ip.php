2971c19df4f26e420789f953d8ac2709b3c77b53
Lukasdotcom <lukas.aurelius@outlook.com>
2021-08-19 11:05:31 -0400

    fixes bug

--- html/include/ip.php
+++ html/include/ip.php
@@ -1,6 +1,6 @@
 <?php
 // Will check if this is requested through cloudflare
-$address = sanitize(apache_request_headers()["CF-Connecting-IP"]);
+$address = sanitize(apache_request_headers()["cf-connecting-ip"]);
 if (! $address) {
     $address = $_SERVER["REMOTE_ADDR"]; // Variable that stores the IP address of user accessing the website
 }
