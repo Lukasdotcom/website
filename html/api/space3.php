0d65e1eaf6910c3a7b6720cec47895b56753357a
Lukasdotcom <lukas.aurelius@outlook.com>
2021-10-31 14:49:41 -0400

    Added a length limit to the api to make it faster.

--- html/api/space3.php
+++ html/api/space3.php
@@ -1,9 +1,16 @@
 <?php
 require_once "api.php";
-if (gettype($OGGET["search"]) == "string") {
+if (gettype($OGGET["search"]) == "string") { // Used for searching the database
     $searchTerm = $OGGET["search"];
     $searchTerm = "%$searchTerm%";
-    echo json_encode(dbRequest2("SELECT * FROM space3 WHERE description LIKE ?", $result="*", $prepare=[$searchTerm]));
+    $defaultLength = 20;
+    $response = dbRequest2("SELECT * FROM space3 WHERE description LIKE ?", $result="*", $prepare=[$searchTerm]);
+    if (intval($_GET["length"])) {
+        $response = array_slice($response, 0, $_GET["length"]);
+    } else {
+        $response = array_slice($response, 0, $defaultLength);
+    }
+    echo json_encode($response);
 } else {
     http_response_code(400);
     echo "Invalid command";
