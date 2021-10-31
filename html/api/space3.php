49ab9259bf841070d1d9e0385b43312edd6ad213
Lukasdotcom <lukas.aurelius@outlook.com>
2021-10-31 14:27:06 -0400

    Added a simple search to the space 3 api.

---
+++ html/api/space3.php
@@ -0,0 +1,10 @@
+<?php
+require_once "api.php";
+if ($OGGET["search"]) {
+    $searchTerm = $OGGET["search"];
+    $searchTerm = "%$searchTerm%";
+    echo json_encode(dbRequest2("SELECT * FROM space3 WHERE description LIKE ?", $result="*", $prepare=[$searchTerm]));
+} else {
+    http_response_code(400);
+    echo "Invalid command";
+}
