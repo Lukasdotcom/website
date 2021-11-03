86d9f878ede09df48f734a6d78dec274180d7a6e
Lukasdotcom <lukas.aurelius@outlook.com>
2021-11-03 18:24:39 -0400

    update and delete preferences

--- html/api/space3.php
+++ html/api/space3.php
@@ -12,7 +12,32 @@ if (gettype($OGGET["search"]) == "string") { // Used for searching the database
         $response = array_slice($response, 0, $defaultLength);
     }
     echo json_encode($response);
-} else {
+} elseif ($_POST["update"] and $USERNAME) { // Used to update or add to the space 3 addons
+    if ($_POST["id"]) {
+        $id = $_POST["id"];
+        if ($OGPOST["preferences"]) { // Will check if the preferences need to be updated
+            $newPreference = $OGPOST["preferences"];
+            echo "Overwrote preference with id $id.";
+        } else {
+            $newPreference = dbRequest2("SELECT preferences FROM space3 WHERE id='$id' and owner='$USERNAME'", $result="preferences");
+            if ($newPreference) {
+                $newPreference = $newPreference[0];
+            } else {
+                http_response_code(401);
+                echo "You do not own this preference.";
+                exit();
+            }
+            echo "Updated description/title for preference with id $id.";
+        }
+        dbCommand("DELETE FROM space3 WHERE id='$id' and owner='$USERNAME'");
+        dbCommand("INSERT INTO space3 (`id`, `owner`, `title`, `description`, `preferences`) VALUES ('$id', '$USERNAME', ?, ?, ?)", $prepare=[$OGPOST["title"], $OGPOST["description"], $newPreference]);
+    } else {
+        dbCommand("INSERT INTO space3 (`owner`, `title`, `description`, `preferences`) VALUES ('$USERNAME', ?, ?, ?)", $prepare=[$OGPOST["title"], $OGPOST["description"], $OGPOST["preferences"]]);
+        echo "Added new preference with id $id";
+    }
+} elseif ($_POST["delete"] and $USERNAME) { // Used to delete a preference
+    dbCommand("DELETE FROM space3 WHERE id='$id' and owner='$USERNAME'");
+}else {
     http_response_code(400);
     echo "Invalid command";
 }
