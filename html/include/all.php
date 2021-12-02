438a16365512a215df3ebc672b17beef964ffb55
Lukasdotcom <lukas.aurelius@outlook.com>
2021-12-02 18:12:24 -0500

    Moves css files to css folder

--- html/include/all.php
+++ html/include/all.php
@@ -9,6 +9,7 @@ echo '<meta http-equiv="content-language" content="en-us">
     <link rel="stylesheet" type="text/css" href="/css/website.css" />
     <script type="text/javascript" src="/javascript/jquery.js"></script>
     <script type="text/javascript" src="/javascript/jquery-ui.js"></script>
+    <link rel="stylesheet" href="/css/jquery-ui.min.css">
     <script type="text/javascript" src="/javascript/functions.js"></script>
     <script type="text/javascript" src="/javascript/cookie.js"></script>';
 if (false) {
