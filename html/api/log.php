f39e00d23a8f0dc6ca9195329e5786f2a53f53b0
Lukasdotcom <lukas.aurelius@outlook.com>
2021-12-04 20:41:31 -0500

    Fixes server log

--- html/api/log.php
+++ html/api/log.php
@@ -6,6 +6,24 @@ if (array_key_exists("data", $_GET)){ // Gets the log
     } else {
         missingPrivilege($USERNAME);
     }
+} elseif (array_key_exists("log", $_GET)) { # Allows for the requesting of specific logs can be used like the old api still just don't give time data and reverse the order.
+    if ($PRIVILEGE["viewLog"]) {
+        if (array_key_exists("startTime", $_GET)) {
+            $startTime = intval($_GET["startTime"]);
+            $startTime = "time>$startTime";
+        } else {
+            $startTime = "time>0";
+        }
+        if (array_key_exists("endTime", $_GET)) {
+            $endTime = intval($_GET["endTime"]);
+            $endTime = "and time<$endTime";
+        } else {
+            $endTime = "";
+        }
+        echo json_encode(dbRequest2("SELECT * FROM log WHERE $startTime $endTime"));
+    } else {
+        missingPrivilege($USERNAME);
+    }
 } elseif (array_key_exists("remove", $_POST) and array_key_exists("time", $_POST) and array_key_exists("message", $_POST)) { // To delete an entry in log
     if ($PRIVILEGE["deleteLog"]) {
         $possibleDelete = dbRequest("message", "log", "time", $_POST["time"], 0);
--- html/api/server.php
+++ html/api/server.php
@@ -16,22 +16,6 @@ if (array_key_exists("uptime", $_GET)) { # Will return some status info for a se
     } else {
         missingPrivilege($USERNAME);
     }
-} elseif (array_key_exists("log", $_GET) and array_key_exists("startTime", $_GET) and array_key_exists("endTime", $_GET)) { # Allows for the requesting of specific logs can be used like the old api still just don't give time data and reverse the order.
-    if ($PRIVILEGE["viewLog"]) {
-        if ($_GET["startTime"]) {
-            $startTime = intval($_GET["startTime"]);
-            $startTime = "time>$startTime";
-        } else {
-            $startTime = "time>0";
-        }
-        if ($_GET["endTime"]) {
-            $endTime = intval($_GET["endTime"]);
-            $endTime = "and time<$endTime";
-        }
-        echo json_encode(dbRequest2("SELECT * FROM log WHERE $startTime $endTime"));
-    } else {
-        missingPrivilege($USERNAME);
-    }
 } elseif (array_key_exists("restart", $_POST)) { // To delete an entry in log
     if ($PRIVILEGE["restartServer"]) {
         echo "Restarting";
