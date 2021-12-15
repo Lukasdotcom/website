74f7e30918cb602b93441c8a7665cfe471d1105c
Lukasdotcom <lukas.aurelius@outlook.com>
2021-12-15 14:50:18 -0500

    Fixes bug with login api that causes it to crash in php 8

--- html/api/login.php
+++ html/api/login.php
@@ -27,31 +27,36 @@ if (array_key_exists("username", $_POST) and array_key_exists("type", $_POST) an
         }
     }
     $RESULT = dbRequest("password", "users", "username", $_POST["username"], 0);
-    $RESULT = $RESULT[0];
-    if ($RESULT and password_needs_rehash($RESULT, PASSWORD_BCRYPT)) {
-        $RESULT2 = $RESULT;
-        $RESULT = password_hash($RESULT, PASSWORD_BCRYPT);
-        dbEdit("users", [["password", $RESULT]], ["password", $RESULT2], 0);
-    }
-    if (password_verify($_POST["password"], $RESULT)) {
-        // If the username and password are valid a cookie entry is put into the database and the cookie is put on the user
-        $USERNAME = $_POST["username"];
-        // logs the fact that a login happened and if a signup happened
-        if ($_POST["type"] == "signup") {
-            writeLog(2, "$USERNAME created by $address");
+    if ($RESULT) {
+        $RESULT = $RESULT[0];
+        if (password_needs_rehash($RESULT, PASSWORD_BCRYPT)) {
+            $RESULT2 = $RESULT;
+            $RESULT = password_hash($RESULT, PASSWORD_BCRYPT);
+            dbEdit("users", [["password", $RESULT]], ["password", $RESULT2], 0);
         }
-        writeLog(0, "$USERNAME was logged in by $address");
-        $Time = time() + 3600*12;
-        $Cookie = $USERNAME;
-        $Cookie .= microtime();
-        $Cookie = sanitize(substr(sha1($Cookie), 5));
-        $CookieForDB = [$Cookie, $USERNAME, $Time];
-        dbAdd($CookieForDB, "cookies");
-        setcookie("user", $Cookie, time() + 600, "/");
-        echo json_encode($Cookie);
+        if (password_verify($_POST["password"], $RESULT)) {
+            // If the username and password are valid a cookie entry is put into the database and the cookie is put on the user
+            $USERNAME = $_POST["username"];
+            // logs the fact that a login happened and if a signup happened
+            if ($_POST["type"] == "signup") {
+                writeLog(2, "$USERNAME created by $address");
+            }
+            writeLog(0, "$USERNAME was logged in by $address");
+            $Time = time() + 3600*12;
+            $Cookie = $USERNAME;
+            $Cookie .= microtime();
+            $Cookie = sanitize(substr(sha1($Cookie), 5));
+            $CookieForDB = [$Cookie, $USERNAME, $Time];
+            dbAdd($CookieForDB, "cookies");
+            setcookie("user", $Cookie, time() + 600, "/");
+            echo json_encode($Cookie);
+        } else {
+            http_response_code(401);
+            echo "Wrong password";
+        }
     } else {
         http_response_code(401);
-        echo "Wrong password or username try again.";
+        echo "User does not exist";
     }
 } else {
     http_response_code(400);
