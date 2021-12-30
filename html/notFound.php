2006eafa8d76bab0da535be8a904efe94dab5349
Lukasdotcom <lukas.aurelius@outlook.com>
2021-12-30 17:56:07 -0500

    Added 404 page

---
+++ html/notFound.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>
+        Schaefer Family
+    </title>
+    <?php
+    $DESCRIPTION = "This page could not be found.";
+    require_once 'include/all.php';
+    ?>
+</head>
+
+<body>
+    <?php
+    include 'include/menu.php';
+    echo "<div class='main'>";
+    ?>
+    <h1>404 Not Found</h1>
+    <h3>Floppy could sadly not find your page :(</h3>
+    <p>She did find some food on the way though.</p>
+    <?php
+    createImage("notFound", "Picture of the great bun bun Floppy finding food.");
+    ?>
+    </div>
+</body>
+
+</html>
