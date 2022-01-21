9d8f052140dc311243ac77e95e9908e319096970
Lukasdotcom <lukas.aurelius@outlook.com>
2022-01-21 17:22:27 -0500

    Added logging for creating and deleting sessions

--- html/api/key.php
+++ html/api/key.php
@@ -7,6 +7,7 @@ if (array_key_exists("get", $_GET)) {
     $key = $_POST["delete"];
     dbCommand("DELETE FROM cookies WHERE username='$USERNAME' and cookie='$key'");
     echo "Deleted session";
+    writeLog(21, "User $USERNAME revoked session with ip $address");
 } else if (array_key_exists("create", $_POST)) {
     $expire = intval($_POST["create"]);
     $cookie = $USERNAME;
@@ -14,6 +15,7 @@ if (array_key_exists("get", $_GET)) {
     $cookie = sanitize(substr(sha1($cookie), 5));
     dbCommand("INSERT INTO cookies VALUES ('$cookie', '$USERNAME', $expire, '$address')");
     echo "Created new session";
+    writeLog(22, "User $USERNAME created session with ip $address");
 } else {
     http_response_code(400);
     echo "Invalid command";
