13ea7b8de1410bca211d564b367c6f8a3de89020
Lukasdotcom <lukas.aurelius@outlook.com>
2022-01-21 16:20:53 -0500

    Added ability to revoke session

--- html/api/key.php
+++ html/api/key.php
@@ -1,8 +1,12 @@
 <?php
 require_once "api.php";
+loggedIn($USERNAME);
 if (array_key_exists("get", $_GET)) {
-    loggedIn($USERNAME);
     echo json_encode(dbRequest2("SELECT * FROM cookies WHERE username='$USERNAME'"));
+} else if (array_key_exists("delete", $_POST)) {
+    $key = $_POST["delete"];
+    dbCommand("DELETE FROM cookies WHERE username='$USERNAME' and cookie='$key'");
+    echo "Deleted session";
 } else {
     http_response_code(400);
     echo "Invalid command";
--- html/usermenu/key.js
+++ html/usermenu/key.js
@@ -7,15 +7,29 @@ function updateKey() { // Used to update the keys and session data
             Object.values(JSON.parse(this.responseText)).forEach(element => {
                 let date = new Date(element["expire"]*1000);
                 expireText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} at ${date.getMonth()+1}-${date.getDate()}-${date.getFullYear()}`;
-                text += `<tr><td>${element["cookie"]}</td><td>${expireText}</td></tr>`;
+                text += `<tr id='${element["cookie"]}' ><td>${element["cookie"]}</td><td>${expireText}</td><td><button onclick='revoke("${element["cookie"]}")'>Revoke</button></td></tr>`;
             });
             $("#keys").html(text);
         } else {
-            JQerror("Could not update the sessions.");
+            JQerror(this.responseText);
         }
     }
     ajax.open("GET", `/api/key.php?get=true&key='${getCookie('user')}'`);
     ajax.send(); 
+}
+
+function revoke(key) { // Used to revoke a key
+    const ajax = new XMLHttpRequest();
+    
+    ajax.onload = function() {
+        if (ajax.status != 200) {
+            JQerror(this.responseText);
+        }
+        updateKey();
+    }
+    ajax.open("POST", `/api/key.php`);
+    ajax.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    ajax.send(`delete=${key}&key='${getCookie('user')}'`); 
 }
 
 $(document).ready(function() {
