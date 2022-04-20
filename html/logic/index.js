ce30295b945286586548525c6780a386b4961afe
Lukasdotcom <lukas@lschaefer.xyz>
2022-04-20 18:34:52 -0400

    Made the parenthesis on the sentence much better

--- html/logic/index.js
+++ html/logic/index.js
@@ -8,47 +8,34 @@ $(document).ready(function() {
 const negationLikelyhood = 0.2
 const upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const logicOperations = ["v", "*", "→", "↔"]
-const parenthesis = [["(", ")"], ["[", "]"], ["{", "}"], ["<b style='font-size:larger'>(</b>", "<b style='font-size:larger'>)</b>"]]
-function generateTruthSentence(complexity) { // Used to generate a logic sentence
-    let truthSentence = ""
-    // The depth in parenthesis
-    let parenthesisNumber = 0
-    // Waits until the logic sentence is completed
-    while (complexity > 0) {
-        if (truthSentence == "") { // If the logic sentence has not been started it either generates a single leader or a simple sentence with 2 letters
-            if (complexity == 1) {
-                if (Math.random() < negationLikelyhood) {
-                    truthSentence =  "~"
-                }
-                truthSentence += randomElement(upperCaseLetters)
-                complexity -= 1
-            } else {
-                if (Math.random() < negationLikelyhood) {
-                    truthSentence +=  "~"
-                }
-                truthSentence += "("
-                if (Math.random() < negationLikelyhood) {
-                    truthSentence +=  "~"
-                }
-                truthSentence += randomElement(upperCaseLetters) + randomElement(logicOperations)
-                if (Math.random() < negationLikelyhood) {
-                    truthSentence +=  "~"
-                }
-                truthSentence += randomElement(upperCaseLetters) + ")"
-                complexity -= 2
-            }
-        } else {
-            // If a truth sentence exists it is expanded with a randomly sized element
-            let size = Math.ceil(Math.random() * complexity)
-            if (parenthesisNumber < parenthesis.length-1) {
-                parenthesisNumber ++
-            }
-            truthSentence += randomElement(logicOperations) + generateTruthSentence(size) + parenthesis[parenthesisNumber][1]
-            truthSentence = parenthesis[parenthesisNumber][0] + truthSentence
-            complexity -= size
-        }
+// Used to return the parenthesis of a certain size
+function parenthesis(size) {
+    if (size < 3) {
+        return [["(", ")"], ["[", "]"], ["{", "}"]][size]
+    } else {
+        return [`<b style='font-size:${(size - 2) * 2 + 18}px'>(</b>`, `<b style='font-size:${(size - 2) * 2 + 18}px'>)</b>`]
     }
-    return truthSentence
+}
+function generateTruthSentence(complexity) { // Used to generate a logic sentence
+    // The depth in parenthesis
+    if (complexity == 1) { // If the size should be one it generates one random letter
+        return [Math.random() < negationLikelyhood ? "~" : "", randomElement(upperCaseLetters)]
+    } else { // Otherwise it will split this in 2 and recursivly generate a sentence
+        let size = Math.ceil(Math.random() * (complexity - 1))
+        let sentence1 = generateTruthSentence(size)
+        let sentence2 = generateTruthSentence(complexity - size)
+        let parenth = -1
+        if (sentence1.length == 5) {
+            parenth = sentence1[1]
+        }
+        if (sentence2.length == 5) {
+            if (parenth < sentence2[1]) {
+                parenth = sentence2[1]
+            }
+        }
+        parenth ++
+        return [Math.random() < negationLikelyhood ? "~" : "", parenth, sentence1, randomElement(logicOperations), sentence2]
+    }
 }
 var firstOperationSentence = ""
 var firstOperationAttempts = 0
@@ -56,8 +43,8 @@ function firstOperation() {
     let complexity = parseInt($("#firstOperationComplexity").val())
     if (complexity < 2) {
         complexity = 2
-    } else if (complexity > 100) {
-        complexity = 100
+    } else if (complexity > 1000) {
+        complexity = 1000
     }
     // Checks if the game has already been played and if this was a completed game sends the event.
     if (firstOperationAttempts != 0) {
@@ -65,24 +52,14 @@ function firstOperation() {
         firstOperationAttempts = 0
     }
     localStorage.firstOperationComplexity = complexity
-    // Generates a sentence
-    sentence = generateTruthSentence(complexity)
-    let depth = 0
-    trueSentence = ""
-    // Finds where the answer for the sentence is and puts buttons on all the operators
-    for (let index = 0; index < sentence.length; index++) {
-        parenthesis.forEach(element => {
-            if (sentence[index] == element[0]) {
-                depth ++
-            } else if (sentence[index] == element[1]) {
-                depth --
-            }
-        })
-        if (logicOperations.includes(sentence[index])) {
-            trueSentence += `<a style='color:green' onClick='firstOperationAttempts ++;if(${depth == 1}) {alert("You are correct"); firstOperation()} else {alert("This is the wrong answer")}'> ${sentence[index]} </a>`
+    // Will generate the text for the truth sentence
+    function generateSentenceText(sentence, first) {
+        if (sentence.length == 2) {
+            return `${sentence[0]}${sentence[1]}`
         } else {
-            trueSentence += sentence[index]
+            return `${sentence[0]}${parenthesis(sentence[1])[0]}${generateSentenceText(sentence[2], false)}<a style='color:green' onClick='firstOperationAttempts ++;if(${first}) {alert("You are correct"); firstOperation()} else {alert("This is the wrong answer")}'> ${sentence[3]} </a>${generateSentenceText(sentence[4], false)}${parenthesis(sentence[1])[1]}`
         }
     }
-    $("#firstOperation").html(trueSentence)
+    // Generates a sentence
+    $("#firstOperation").html(generateSentenceText(generateTruthSentence(complexity), true))
 }
