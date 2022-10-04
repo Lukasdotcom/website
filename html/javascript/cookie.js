3cc678f248fef44c1b01175d010de941cfa7bb31
Lukas <lukas@lschaefer.xyz>
2022-10-04 18:17:34 -0400

    Fixes typo with Mastodon instead of matomo

--- html/javascript/cookie.js
+++ html/javascript/cookie.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         div.innerHTML = `<div class='popup-content'>
                             <h1>Cookie Policy</h1>
                             <p>This website uses cookies to function properly. Most cookies are strictly functional and your preferences will never be saved on our server. This also uses Cloudflare's CDN. If you want to see exactly what the cookies are used for <a href="https://github.com/Lukasdotcom/website" target="_blank" rel="noopener noreferrer" >click here</a> to see the source code of the website.
-                            This website also uses mastodon you may disable mastodon analytics by clicking this <a href="https://analytics.lschaefer.xyz/index.php?module=CoreAdminHome&action=optOut&language=en&backgroundColor=000000&fontColor=ffffff&fontSize=17px&fontFamily=" target="_blank" rel="noopener noreferrer" >link</a> and unchecking the check box.</p>
+                            This website also uses matomo you may disable matomo analytics by clicking this <a href="https://analytics.lschaefer.xyz/index.php?module=CoreAdminHome&action=optOut&language=en&backgroundColor=000000&fontColor=ffffff&fontSize=17px&fontFamily=" target="_blank" rel="noopener noreferrer" >link</a> and unchecking the check box.</p>
                             <button id='accept_cookies'>Click here to accept functional cookies</button>
                             <div class="red"><button id='decline_cookies'>Click here to decline all cookies and go back to previous page</button></div>
                         </div>`;
