9cce60ed3722637f237080e492048eacccd9bc4a
Lukasdotcom <lukas@lschaefer.xyz>
2023-11-01 14:09:43 -0400

    Fixes capitilization of header

--- html/include/ip.php
+++ html/include/ip.php
@@ -1,8 +1,8 @@
 <?php
 // Will check if this is requested through cloudflare
-if (array_key_exists("cf-connecting-ip", apache_request_headers())) {
-    $address = sanitize(apache_request_headers()["cf-connecting-ip"]);
-    if (! $address) {
+if (array_key_exists("Cf-Connecting-Ip", apache_request_headers())) {
+    $address = sanitize(apache_request_headers()["Cf-Connecting-Ip"]);
+    if (!$address) {
         $address = $_SERVER["REMOTE_ADDR"]; // Variable that stores the IP address of user accessing the website
     }
 } else {
