0093b81262f3fbdeaf456f02f0017dea01355587
Lukasdotcom <lukas@lschaefer.xyz>
2024-08-24 19:57:32 -0400

    Adds minecraft server page

---
+++ html/minecraft.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html dir="ltr" lang="en">
+
+<head>
+  <title>
+    Minecraft Server
+  </title>
+  <?php
+  $DESCRIPTION = "Minecraft Server hosted by Lukas.";
+  require_once 'include/all.php';
+  ?>
+</head>
+
+<body>
+  <?php
+  include 'include/menu.php';
+  ?>
+  <div class='main'>
+    <h1>Minecraft Server</h1>
+    <p>Server IP: minecraft.lschaefer.xyz</p>
+    <p>Minecaft Version: 1.21.1</p>
+    <h2>Client Side Mods to Install</h2>
+    <li><a href="https://modrinth.com/plugin/simple-voice-chat">Simple Voice Chat</a> - Allows you to have a voice chat with anyone on the server that also has that mod installed.</li>
+    <li><a href="https://modrinth.com/mod/better-stats">Better Stats</a> - Allows you to see detailed stats for all players.</li>
+    <h2>Mod List</h2>
+    <li><a href="https://modrinth.com/mod/lithium">lithium</a></li>
+    <li><a href="https://modrinth.com/mod/simple-voice-chat">simple-voice-chat</a></li>
+    <li><a href="https://modrinth.com/mod/dungeons-and-taverns">dungeons-and-taverns</a></li>
+    <li><a href="https://modrinth.com/mod/dynamic-lights">dynamic-lights</a></li>
+    <li><a href="https://modrinth.com/mod/ct-overhaul-village">ct-overhaul-village</a></li>
+    <li><a href="https://modrinth.com/mod/stack-refill">stack-refill</a></li>
+    <li><a href="https://modrinth.com/mod/immersive-structures">immersive-structures</a></li>
+    <li><a href="https://modrinth.com/mod/tidal-towns">tidal-towns</a></li>
+    <li><a href="https://modrinth.com/mod/fast-minecart">fast-minecart</a></li>
+    <li><a href="https://modrinth.com/mod/terralith">terralith</a></li>
+    <li><a href="https://modrinth.com/mod/tectonic">tectonic</a></li>
+    <li><a href="https://modrinth.com/mod/ships">ships</a></li>
+    <li><a href="https://modrinth.com/mod/special-mobs">special-mobs</a></li>
+    <li><a href="https://modrinth.com/mod/tick-stasis">tick-stasis</a></li>
+    <li><a href="https://modrinth.com/mod/deeper_dark">deeper_dark</a></li>
+    <li><a href="https://modrinth.com/mod/qraftys-end-villages">qraftys-end-villages</a></li>
+    <li><a href="https://modrinth.com/mod/qraftyfied">qraftyfied</a></li>
+    <li><a href="https://modrinth.com/mod/repurposed-structures-fabric">repurposed-structures-fabric</a></li>
+    <li><a href="https://modrinth.com/mod/explorify">explorify</a></li>
+    <li><a href="https://modrinth.com/mod/tabtps">tabtps</a></li>
+    <li><a href="https://modrinth.com/mod/better-stats">better-stats</a></li>
+    <li><a href="https://modrinth.com/mod/welcome-message">welcome-message</a></li>
+    <p>The following are only dependencies of the previous mods.</p>
+    <li><a href="https://modrinth.com/mod/fabric-api">fabric-api</a></li>
+    <li><a href="https://modrinth.com/mod/lithostitched">lithostitched</a></li>
+    <li><a href="https://modrinth.com/mod/collective">collective</a></li>
+    <li><a href="https://modrinth.com/mod/tcdcommons">tcdcommons</a></li>
+    <h2>Vanilla Tweaks</h2>
+    <li><a href="https://vanillatweaks.net/share#84cmdb">Share code: 84cmdb</a></li>
+    <li><a href="https://vanillatweaks.net/share#U6lNn9">Share code: U6lNn9</a></li>
+  </div>
+</body>
+
+</html>
